a4f3a3a456c63bc391acbef7790db2cd10b4ba9e
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2016-10-28 15:56:38 -0200

    adicioando projeto java eclipse

---
+++ codigo_fonte_eclipe/src/img_magic/bin/Main.java
@@ -0,0 +1,13 @@
+package img_magic.bin;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("App is runnig...");
+	}
+
+}
---
+++ codigo_fonte_eclipe/src/img_magic/entidades/Cliente.java
@@ -0,0 +1,70 @@
+package img_magic.entidades;
+
+public class Cliente {
+	private String nome;
+	private String sobrenome;
+	private String cpf;
+	private String rua;
+	private Integer numero;
+	private String bairro;
+	private String cep;
+	private String estado;
+	private String pais;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	
+}
